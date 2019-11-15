e4e2b97e620c34754492a1fde597efb7e75b1387
Vash <sakesahi@gmail.com>
2019-11-15 00:02:32 -0500

    subiendo

---
+++ src/holamundo/Holamundo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Scanner inicio = new Scanner (System.in);
+      double numero ;
+        System.out.println("Digite un numero: ");
+        numero = inicio.nextDouble();
+        System.out.println("El numero es: "+numero);
+        
+    }
+    
+}
